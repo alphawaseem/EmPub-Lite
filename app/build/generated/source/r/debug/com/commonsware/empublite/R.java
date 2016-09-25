5016c440b2fb73491eba5724953bed280dce2b59
Waseem Ahmed <alphawaseem@gmail.com>
2016-09-25 18:18:21 +0530

    add menu

--- app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -10,21 +10,36 @@ package com.commonsware.empublite;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int ic_action_about=0x7f020000;
+        public static final int ic_action_discard=0x7f020001;
+        public static final int ic_action_edit=0x7f020002;
+        public static final int ic_action_help=0x7f020003;
+        public static final int ic_action_refresh=0x7f020004;
+        public static final int ic_action_settings=0x7f020005;
+    }
     public static final class id {
-        public static final int progressBar=0x7f060000;
+        public static final int about=0x7f080002;
+        public static final int help=0x7f080001;
+        public static final int progressBar=0x7f080000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int main=0x7f040000;
+    }
+    public static final class menu {
+        public static final int options=0x7f070000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int about=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int help=0x7f050002;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,11 +2,42 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EmPubLiteActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.help:
+                help();
+                break;
+            case R.id.about:
+                about();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void help() {
+
+    }
+
+    private void about() {
+
     }
 }
