d5d8edd1c98e7772693ded14d73b7d155f37d582
Waseem Ahmed <alphawaseem@gmail.com>
2016-10-02 14:04:44 +0530

    finish part 14

--- app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -24,17 +24,22 @@ public final class R {
         public static final int ic_action_settings=0x7f020005;
     }
     public static final class id {
-        public static final int about=0x7f0a0003;
-        public static final int help=0x7f0a0002;
-        public static final int pager=0x7f0a0001;
-        public static final int progressBar1=0x7f0a0000;
-        public static final int settings=0x7f0a0004;
+        public static final int about=0x7f0a0006;
+        public static final int delete=0x7f0a0003;
+        public static final int editor=0x7f0a0000;
+        public static final int help=0x7f0a0005;
+        public static final int notes=0x7f0a0004;
+        public static final int pager=0x7f0a0002;
+        public static final int progressBar1=0x7f0a0001;
+        public static final int settings=0x7f0a0007;
     }
     public static final class layout {
-        public static final int main=0x7f040000;
+        public static final int editor=0x7f040000;
+        public static final int main=0x7f040001;
     }
     public static final class menu {
-        public static final int options=0x7f090000;
+        public static final int notes=0x7f090000;
+        public static final int options=0x7f090001;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
@@ -42,12 +47,15 @@ public final class R {
     public static final class string {
         public static final int about=0x7f080000;
         public static final int app_name=0x7f080001;
-        public static final int help=0x7f080002;
-        public static final int keepscreenon_summary=0x7f080003;
-        public static final int keepscreenon_title=0x7f080004;
-        public static final int lastposition_summary=0x7f080005;
-        public static final int lastposition_title=0x7f080006;
-        public static final int settings=0x7f080007;
+        public static final int delete=0x7f080002;
+        public static final int help=0x7f080003;
+        public static final int hint=0x7f080004;
+        public static final int keepscreenon_summary=0x7f080005;
+        public static final int keepscreenon_title=0x7f080006;
+        public static final int lastposition_summary=0x7f080007;
+        public static final int lastposition_title=0x7f080008;
+        public static final int notes=0x7f080009;
+        public static final int settings=0x7f08000a;
     }
     public static final class style {
         /**  Customize your theme here. 
